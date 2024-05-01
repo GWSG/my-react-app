89dd4c8887a6db8dd140e9e077a41beea2a545ef
GWSG <henryhsu0822@gmail.com>
2024-05-01 20:14:23 +0800

    up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from "./logo.svg";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import logo from "./logo.svg"; // 如果需要顯示 logo，可以取消這部分的註解
 import "./App.css";
 import MySlider from "./MySlider";
 
 function App() {
   return (
     <div className="App">
-      <h1>Javascript程式設計，使用react</h1>
+      <h1>Javascript程式設計，學號5B0G0042，姓名:許皓詠</h1>
       <h2>第一個元件</h2>
       <span>R: </span><MySlider />
       <span>G: </span><MySlider />
@@ -26,5 +28,23 @@ function App() {
     </div>
   );
 }
+
+function Football() {
+  const shoot = () => {
+    alert("Great Shot!");
+  }
+
+  return (
+    <button onClick={shoot}>Take the shot!</button>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+    <Football />
+  </React.StrictMode>
+);
 
 export default App;
