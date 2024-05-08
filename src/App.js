e56f1a62b5dfc60077ea8380579865d64c94320a
GWSG <henryhsu0822@gmail.com>
2024-05-08 13:08:47 +0800

    up

--- src/App.js
+++ src/App.js
@@ -16,19 +16,6 @@ function App() {
       <span>R: </span><MySlider />
       <span>G: </span><MySlider />
       <span>B: </span><MySlider />
-      {/* <header className="App-header"> */}
-      {/* <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello React! 2024.5.1</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-
-      {/* </header> */}
       {/* 新增按鈕 */}
       <button onClick={handleClick} className="button-large">Button</button>
     </div>
