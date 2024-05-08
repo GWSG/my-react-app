0068656300c4579ff872622c5e21b14fa7fe0e5f
GWSG <henryhsu0822@gmail.com>
2024-05-08 14:12:15 +0800

    up

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import "./App.css";
 import MyRGBPanel from "./components/MyRGBPanel";
 
 function App() {
+  // 點擊事件處理函數
+  const handleClick = () => {
+    console.log("按鈕被點擊了！");
+  };
+
   return (
     <div className="App">
       <h1>Javascript程式設計，使用react</h1>
       <h2>第一個元件</h2>
-      <button onClick={handleClick} className="button-large">Button</button>
       <MyRGBPanel />
+      <button onClick={handleClick} className="button-large">Button</button>
     </div>
   );
 }
