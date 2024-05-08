1da9b130b37bead96d929b510985f6b023204852
GWSG <henryhsu0822@gmail.com>
2024-05-08 14:00:21 +0800

    up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from "./logo.svg";
 import "./App.css";
-import MySlider from "./MySlider";
-
+import MyRGBPanel from "./components/MyRGBPanel";
 
 function App() {
-
-  const handleClick = () => {
-    alert('HELLO，我是學號：5B0G0042，姓名：許皓詠');
-  }
-
   return (
     <div className="App">
-      <h1>Javascript程式設計，學號5B0G0042，姓名:許皓詠</h1>
+      <h1>Javascript程式設計，使用react</h1>
       <h2>第一個元件</h2>
-      <span>R: </span><MySlider />
-      <span>G: </span><MySlider />
-      <span>B: </span><MySlider />
+      <MyRGBPanel />
       {/* 新增按鈕 */}
       <button onClick={handleClick} className="button-large">Button</button>
+
     </div>
   );
 }
