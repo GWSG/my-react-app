d47b24a5e73a4e7fb412413dfe8cbf7913dcfcfb
GWSG <henryhsu0822@gmail.com>
2024-05-08 14:02:14 +0800

    up

--- src/App.js
+++ src/App.js
@@ -7,8 +7,6 @@ function App() {
       <h1>Javascript程式設計，使用react</h1>
       <h2>第一個元件</h2>
       <MyRGBPanel />
-      {/* 新增按鈕 */}
-      <button onClick={handleClick} className="button-large">Button</button>
 
     </div>
   );
