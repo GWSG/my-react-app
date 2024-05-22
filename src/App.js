6a5fdba4b1ba618bf51aaa2958686b5d32a5cc11
GWSG <henryhsu0822@gmail.com>
2024-05-22 15:00:45 +0800

    up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import MyRGBPanel from "./components/MyRGBPanel";
-import MyCalculator from "./components/MyCalculator"
-
+import MySlider from "./components/MySlider";
+import MyCalculator from "./components/MyCalculator";
 function App() {
  // 點擊事件處理函數，彈出訊息框
   const handleClick = () => {
@@ -14,6 +14,9 @@ function App() {
       <MyRGBPanel />
       <h2>第二個元件</h2>
       <button onClick={handleClick} className="button-large">緊急按鈕</button>
+      <h1>MySlider</h1>
+      <MySlider />
+  
       <h1>MyCalculator</h1>
       <MyCalculator />
     </div>
