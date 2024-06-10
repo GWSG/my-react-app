cff4ce24286534edd4a3bd1ac8518c8cb1f70113
GWSG <henryhsu0822@gmail.com>
2024-06-10 17:36:45 +0800

    up

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import "./App.css";
-import MyRGBPanel from "./components/rgb-panel/MyRGBPanel"
-import MySlider from "./components/rgb-panel/MySlider";
-import MyCalculator from "./components/calculator/MyCalculator";
-import TicTacToe from "./components/tic-tac-toe/TicTacToe";
+import "./App.css"; // 引用App.css的設計樣式
+import MyRGBPanel from "./components/rgb-panel/MyRGBPanel" //引用components的rgb-panel的MyRGBPanel.js檔
+import MySlider from "./components/rgb-panel/MySlider"; //同上
+import MyCalculator from "./components/calculator/MyCalculator"; //同上
+import TicTacToe from "./components/tic-tac-toe/TicTacToe"; //同上
 
 function App() {
  // 點擊事件處理函數，彈出訊息框
@@ -16,14 +16,14 @@ function App() {
       <h2>第一個元件</h2>
       <MyRGBPanel />
       <h2>第二個元件</h2>
-      <button onClick={handleClick} className="button-large">緊急按鈕</button>
+      <button onClick={handleClick} className="button-large">緊急按鈕</button> 
       
-      <h1>MySlider</h1>
+      <h1>MySlider</h1> 
       <MySlider />
   
       <h1>MyCalculator</h1>  
-       
       <MyCalculator />
+
       <h1>Tic-Tac-Toe Game</h1>
       <TicTacToe />
       
