3b7efd0e3544c73921829084364be68e25028373
GWSG <henryhsu0822@gmail.com>
2024-06-14 23:01:08 +0800

    up

--- src/components/calculator/MyDisplay.js
+++ src/components/calculator/MyDisplay.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function MyDisplay(props){
-    return <div className='display'>{props.result}</div>
+    return <div className='display'>{props.result}</div>//這個主要顯示計算結果後的答案,就是顯示答案的小黑板
 }
 
 export default MyDisplay;
