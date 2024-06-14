6cbc3d3535b922ed928296d26eafbf6f8478be65
GWSG <henryhsu0822@gmail.com>
2024-06-14 22:08:35 +0800

    up

--- src/components/rgb-panel/MyRGBPanel.js
+++ src/components/rgb-panel/MyRGBPanel.js
@@ -3,47 +3,49 @@ import { useState } from "react"; //紀錄每一個滑軌所移動的值
 import MySlider from "./MySlider"; //引用MySlider.js
 
 function MyRGBPanel() {
-    const [r, setR] = useState(128);//有一個變數是r
-    const [g, setG] = useState(128);
-    const [b, setB] = useState(128);
+    const [r, setR] = useState(128);//有一個變數是r,如果有收到調整的值,就傳到方塊和方框那邊去
+    const [g, setG] = useState(128);//有一個變數是g,如果有收到調整的值,會傳到方塊和方框那邊去
+    const [b, setB] = useState(128);//有一個變數是b,如果有收到調整的值,會傳到方塊和方框那邊去
 
-    const updateR = (v) => setR(v); //傳R的值到setR
-    const updateG = (v) => setG(v); //傳G的值到setG
-    const updateB = (v) => setB(v); //傳B的值到setB
+    const updateR = (v) => setR(v); //如果R滑軌有移動的話,就傳R的值到setR
+    const updateG = (v) => setG(v); //如果G滑軌有移動的話,就傳G的值到setG
+    const updateB = (v) => setB(v); //如果B滑軌有移動的話,就傳B的值到setB
 
     
 
-    const rgbCss = {
-        border: `10px solid rgb(${r}, ${g}, ${b})`, // 設定邊框顏色為動態 RGB 值
-        borderRadius: "20px",
-        width: "300px",
-        display: "flex",
-        flexDirection: "column",
-        padding: "20px",
-        justifyContent: "center",
-        alignItems: "center",
-        margin: "auto",
-    };
+// 定義一個名為 rgbCss(可以變顏色的方框) 的常數對象
+const rgbCss = {
+    border: `10px solid rgb(${r}, ${g}, ${b})`, // 邊框顏色
+    borderRadius: "20px", // 邊框圓角
+    width: "300px", // 寬度
+    display: "flex", // Flex 彈性的意思(置中對其)
+    flexDirection: "column", // Flex 方向
+    padding: "20px", // 內邊距
+    justifyContent: "center", // 主軸居中(置中對其)
+    alignItems: "center", // 交叉軸居中
+    margin: "auto", // 整個rgbCss的元件水平居中
+};
+
 
     return (
         <>
               {/* 顯示外圍方框的背景色 */}
             <div style={rgbCss}>
                 {/* 顯示目前色彩 */}
-                <h2 style={{ color:`rgb(${r}, ${g}, ${b})`}}>
+                <h2 style={{ color:`rgb(${r}, ${g}, ${b})`}}> {/* 會隨著改變r,g,b這三的變數的數值,這個方框也會跟著變色 */}
                     目前色彩
                 </h2>
 
                   {/* 顯示方框裡面的長方形區塊的背景色 */}
                 <div
                     style={{
-                        width: "250px",
-                        height: "100px",
-                        backgroundColor: `rgb(${r}, ${g}, ${b})`,
+                        width: "250px", //寬度
+                        height: "100px",//高度
+                        backgroundColor: `rgb(${r}, ${g}, ${b})`,//會隨著改變r,g,b這三的變數的數值,這個方塊也會跟著變色
                     }}
                 />
                 <span>R: </span> {/* span是顯示在網頁上的字體 */} 
-                <MySlider value={r} onChange={updateR} />{/* value是顯示在網頁上滑軌移動的數值 */}
+                <MySlider value={r} onChange={updateR} />{/* value是顯示在網頁上滑軌移動的數值,如果有移動的話就呼叫updateR */}
                 <span>G: </span>
                 <MySlider value={g} onChange={updateG} />
                 <span>B: </span>
--- src/components/rgb-panel/MySlider.js
+++ src/components/rgb-panel/MySlider.js
@@ -4,8 +4,8 @@ function MySlider({onChange}) {
   const [value, setValue] = useState(128);// 取得滑軌的狀態並設定滑軌當下的狀態,value是取得狀態會傳給setValue,setValue是設定當下狀態
   const handleChange = (e) => {
     setValue(e.target.value);//如果滑軌的值有被移動了,那就會觸發 onChange ,就會讓value的值改變
-    if (onChange){
-      onChange(e.target.value);
+    if (onChange){//如果onChange有改變就
+      onChange(e.target.value); // 執行 onChange 並傳入新的值
     }
   };
   return (
