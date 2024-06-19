2a3b3084926678e4ff10a281f71b029bb68a616d
GWSG <henryhsu0822@gmail.com>
2024-06-19 12:15:21 +0800

    up

--- src/components/tic-tac-toe/Board.js
+++ src/components/tic-tac-toe/Board.js
@@ -5,7 +5,12 @@ import "./tictactoe.css";
 function Board({ xIsNext, squares, onPlay }) {
   const handleClick = (i) => {
     //然後這邊的handleClick的i就會收到被按下格子的值
+
     if (calculateWinner(squares) || squares[i]) {
+      //如果有人贏,救回傳值,然後i值有東西的話就結束了
+      //i值就是說有發現到玩家點的格子有連成一線的樣子
+      //這樣遊戲就結束了,剩下的格子也不能按了
+
       return;
     }
     const nextSquares = squares.slice();
@@ -21,6 +26,7 @@ function Board({ xIsNext, squares, onPlay }) {
   };
 
   const calculateWinner = (squares) => {
+    //計算誰贏了的程式,用傳進來的squares來判斷
     const lines = [
       [0, 1, 2],
       [3, 4, 5],
@@ -33,16 +39,21 @@ function Board({ xIsNext, squares, onPlay }) {
       [2, 4, 6]
     ];
     for (let i = 0; i < lines.length - 1; i++) {
+      //從lines的所有可能來比較
       const [a, b, c] = lines[i];
+      //a等於0,b等於1,c等於2,這個叫解構,解構的指定值
       if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c])
+        //如果傳值,然後跟a跟b跟c相同
         return squares[a];
     }
     return null;
   };
 
   const winner = calculateWinner(squares);
+
+
   let status = winner ? `贏家: ${winner}` : `下一個玩家: ${xIsNext ? "X" : "O"}`;
-  //每一次都會先從X開始,再來才是O
+  //每一次都會先從X開始,再來才是O,這就是三元運算
 
 
   // let status;
--- src/components/tic-tac-toe/TicTacToe.js
+++ src/components/tic-tac-toe/TicTacToe.js
@@ -5,12 +5,14 @@ import "./tictactoe.css";
 function TicTacToe() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   //history是有兩個1*9的Array
-  //顯示歷程記錄,因為是九個格子,所以元素填九
+  //儲存歷程記錄,把紀錄儲存成history,然後因為是九個格子,所以Array填九
 
   const [currentMove, setCurrentMove] = useState(0);
   //紀錄currentMove現在是到第幾數,初始從0開始
 
   const currentSquares = history[currentMove];
+  //把記錄到的history儲存到currentSquares
+
   const xIsNext = currentMove % 2 === 0;//紀錄現在已經點過幾個格子
   //xIsNext判斷現在是輪到哪個玩家玩
   //如果是'024就換到X玩,如果是135就是O玩,數字的意思是次數,第1次,第2次這樣
@@ -52,8 +54,10 @@ function TicTacToe() {
     <div className="game">
       <div className="game-board">
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
-        {/*Board會傳目前的squares,onPlay是繼續再做*/}
-        {/*xIsNext則是現在按下去的格子是要顯示O還是X*/}
+        {/*把xIsNext和squares跟onPlay的數值傳給Board*/}
+        {/*xIsNext是會傳現在輪到X還是O*/}
+        {/*squares會傳currentSquares的狀態*/}
+        {/*onPlay則是會把是否還有再繼續玩的狀態給Board*/}
       </div>
       <div className="game-info">
         <h4>遊戲歷程</h4>
